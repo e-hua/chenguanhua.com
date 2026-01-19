1a334f92a8f2e0cdf30d1ca780ac1bd7cb194a17
e-hua <eclipsehua@gmail.com>
2026-01-19 16:47:07 +0800

    fix(test): render the App component in the ThemeProvider

--- src/tests/index.test.tsx
+++ src/tests/index.test.tsx
@@ -2,9 +2,14 @@ import { beforeEach, describe, expect, it } from 'vitest'
 import { render } from 'vitest-browser-react'
 import { page } from 'vitest/browser'
 import { App } from '@/App'
+import { ThemeProvider } from '@/hooks/useTheme'
 
 beforeEach(() => {
-  render(<App />)
+  render(
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>,
+  )
 })
 
 describe('Homepage ', () => {

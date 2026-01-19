f0dea18ef3a629ae3d5a50bb3fabf796623a408e
e-hua <eclipsehua@gmail.com>
2026-01-19 12:23:57 +0800

    feat(ui): change the homepage to use new theme as background

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,24 +2,35 @@ import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
-import Header from '../components/Header'
-
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Header />
-      <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
+      <div className="min-h-screen w-full">
+        <div className="fixed inset-0 bg-bg-primary" />
+        {/*
+        Use a relative wrapper around the <Outlet/> component.
+        Since they're both positioned elements, 
+        the browser uses the "Painter's Algorithm" (DOM Order) 
+        to determine which one is on top 
+
+        In this case, the container of Outlet comes last, 
+        hence it's on the top
+         */}
+        <div className="relative">
+          <Outlet />
+        </div>
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+      </div>
     </>
   ),
 })

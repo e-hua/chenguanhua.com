9c707ceb54f81d4d0679adbfe00b3a7e64df124b
e-hua <eclipsehua@gmail.com>
2026-01-20 11:02:06 +0800

    feat(ui): implement the section component

---
+++ src/components/HomeSections/Section.tsx
@@ -0,0 +1,22 @@
+type SectionProps = {
+  id: string
+  title: string
+  children: React.ReactNode
+}
+
+function Section({ id, title, children }: SectionProps) {
+  return (
+    <div
+      id={id}
+      className="
+      scroll-mt-12 text-text-primary text-left 
+      flex flex-col 
+      gap-5 "
+    >
+      <h1 className="font-sans font-semibold text-2xl md:text-3xl">{title}</h1>
+      {children}
+    </div>
+  )
+}
+
+export default Section

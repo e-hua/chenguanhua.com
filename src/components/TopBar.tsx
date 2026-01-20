deaec65f6883e3bbaf18a4412dc7712decb855bf
e-hua <eclipsehua@gmail.com>
2026-01-20 11:01:14 +0800

    feat(ui): modify the top bar component to make it take in the ids of active sections and highlight them

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,24 +1,28 @@
 import { Moon, Sun } from 'lucide-react'
+import { Link } from '@tanstack/react-router'
 import type { ClassValue } from 'clsx'
 import { useTheme, useThemeSetter } from '@/hooks/useTheme'
 import { cn } from '@/lib/utils/cn'
+import { TOPBAR_CONTENT_DATA } from '@/App'
 
 function TopBarButton({
   children,
   className,
   border,
   onClick,
+  hashId,
+  active,
 }: {
   children: React.ReactNode
   className?: ClassValue
   border?: boolean
   onClick?: () => void
+  hashId?: string
+  active?: boolean
 }) {
-  return (
-    <div className="flex flex-row items-center gap-2" onClick={onClick}>
-      <button
-        className={cn(
-          `
+  const isLink = !!hashId
+  const newClassName = cn(
+    `
                 bg-transparent text-center 
                 px-5 py-2 
                 mix-blend-lighten 
@@ -26,19 +30,30 @@ function TopBarButton({
                 rounded-full
                 transition-color duration-300
               `,
-          className,
-        )}
-      >
-        {children}
-      </button>
+    className,
+    active ? 'text-text-primary bg-surface' : '',
+  )
+
+  return (
+    <div className="flex flex-row items-center gap-2" onClick={onClick}>
+      {isLink ? (
+        <Link className={newClassName} to={'/'} hash={hashId}>
+          {children}
+        </Link>
+      ) : (
+        <button className={newClassName}> {children} </button>
+      )}
       {border ? <div className="h-5 w-px bg-surface" /> : <></>}
     </div>
   )
 }
 
-function TopBar() {
-  const sections = ['About me', 'Links', 'My tech stack']
-
+// The hash and smooth scrolling between sections is provided by TanStack router
+/* 
+Inspired by the Toc component by TanStack.com 
+https://github.com/TanStack/tanstack.com/blob/main/src/components/Toc.tsx
+*/
+function TopBar({ visibleSectionIds }: { visibleSectionIds: Array<string> }) {
   const theme = useTheme()
   const setTheme = useThemeSetter()
 
@@ -52,10 +67,15 @@ function TopBar() {
       rounded-full overflow-hidden 
       p-1 gap-2"
       >
-        {sections.map((section, idx) => {
+        {TOPBAR_CONTENT_DATA.map((section, idx) => {
           return (
-            <TopBarButton key={idx} border>
-              {section}
+            <TopBarButton
+              key={idx}
+              border
+              hashId={section.id}
+              active={visibleSectionIds.includes(section.id)}
+            >
+              {section.title}
             </TopBarButton>
           )
         })}

1b8653783fbe706a99220c2965bb37d1ceb5a7f9
e-hua <eclipsehua@gmail.com>
2026-01-28 11:37:48 +0800

    fix(ui): Modify the positioning of top bar

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   const visibleSectionIds = useVisibleSectionIds(TOPBAR_CONTENT_DATA)
 
   return (
-    <div className="w-full text-center relative">
+    <div className="w-auto text-center relative">
       <div className="pt-20 relative flex flex-col gap-20">
         <HeroSection />
         <TechStackSection />
@@ -25,7 +25,7 @@ function App() {
      Let the topbar show up last in the DOM 
      So it gets to be at the top of the stacking context
      */}
-      <div className="fixed top-5 w-full px-5">
+      <div className="fixed top-5 right-5">
         <TopBar visibleSectionIds={visibleSectionIds} />
       </div>
     </div>
